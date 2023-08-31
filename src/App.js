3de21f083af55c7d761c0a1b2fbb9aaf3076fd53
Dmytro <harss320@gmail.com>
2023-08-31 18:59:05 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
           <Route path="/home" element={<HomePage />} />
           <Route path="/about" element={<AboutPage />} />
           <Route path="/contact" element={<ContactPage />} />
-          <Route path="*" element={<NoPage />} />
+          <Route path="/*" element={<NoPage />} />
         </Routes>
       </BrowserRouter>
     </div>
